0359d7ecedf6e86f7000587775486014e4ff8e42
Oliver Ni <oliver.ni@gmail.com>
2022-12-03 13:18:51 -0800

    Add automatic input fetcher

--- run.py
+++ run.py
@@ -27,6 +27,14 @@ if __name__ == "__main__":
     parser.add_argument("--extra", "-e", help="Choose a different solution to run.")
     args = parser.parse_args()
 
+    try:
+        from aocd import get_data
+    except ImportError:
+        pass
+    else:
+        with open(f"input/{args.year}/day{args.day:02}.txt", "w") as f:
+            f.write(get_data(day=args.day, year=args.year))
+
     module_name = f"py.{args.year}.day{args.day:02}"
     if args.extra:
         module_name += f"_{args.extra}"
